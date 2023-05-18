5473eb78c3badcac5d5777d9b377d7bf22b89666
Galaxy-420 <nim.mandakini@gmail.com>
2023-05-18 22:54:44 +0530

    need

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const wrapper = document.querySelector('.wrapper');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
 const btnPop = document.querySelector('.btnLogin-popup');
+const selectImage = doucment.querySelector('.select-image');
+const inputFile = doucment.querySelector('#file');
+const imgArea = doucment.querySelector('.img-area');
 
 const iconClose = document.querySelector('.icon-close');
 
@@ -20,3 +23,29 @@ iconClose.addEventListener('click',()=>{
     wrapper.classList.remove('active-popup ');
 });
 
+selectImage.addEventListener('click', function ()
+{
+    inputFile.click();
+})
+
+inputFile.addEventListener('change' , function(){
+    const image = this.files[0]
+    console.log(image);
+    if(image.size<5000000){
+        const reader = new FileReader();
+        reader.onload = ()=>{
+            const allImg = imgArea.querySelectorAll('img');
+            allImg.forEach(item=> item.remove());
+            const imgUrl = reader.result;
+            const img = document.createElement('img');
+            img.src = imgUrl;
+            imgArea.appendChild(img);
+            imgArea.classList.add('active');
+            imgArea.dataset.img = image.name;
+
+        }
+        reader.readAsDataURL(image);
+    }else{
+        alert("Image size more than 5MB");
+    }
+})
